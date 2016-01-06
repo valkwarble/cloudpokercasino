7e422337ad7d59f21c5dc4feffdac74a19d23832
juan <huertas@mit.edu>
2016-01-06 11:04:24 -0500

    first commit

---
+++ doodle.js
@@ -0,0 +1,36 @@
+if (Meteor.isClient) {
+  
+  Template.hello.onRendered(function() {
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+     $('.slideit').hover(function() {
+       $('.slide').toggleClass('onHover');   
+
+    });
+     $('.typeform-share').hover(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}
+   });
+  });
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    // code to run on server at startup
+  });
+}
+
